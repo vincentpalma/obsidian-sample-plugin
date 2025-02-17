d08054e4fa1c3f23947970f4b386cb8461d70d20
vincentpalma <vincentpalma2001@gmail.com>
2025-02-17 13:53:59 +0100

    bring back jquery style methods, add basic command support

--- main.ts
+++ main.ts
@@ -15,7 +15,6 @@ export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		// console.log("[Plugin] onload");
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
@@ -28,13 +27,11 @@ export default class MyPlugin extends Plugin {
 			}
 		);
 		// Perform additional things with the ribbon
-		// ribbonIconEl.addClass('my-plugin-ribbon-class'); // TODO: this doesn't work, figure out how to add these jquery style functions to HTMLElements
-		ribbonIconEl.classList.add("my-plugin-ribbon-class");
+		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
-		statusBarItemEl.textContent = "Status Bar Text";
+		statusBarItemEl.setText('Status Bar Text');
 
 		const modal = new SampleModal(this.app);
 		this.addRibbonIcon("dice", "Open modal", (evt: MouseEvent) => {
@@ -42,13 +39,13 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		//   id: 'open-sample-modal-simple',
-		//   name: 'Open sample modal (simple)',
-		//   callback: () => {
-		//     new SampleModal(this.app).open();
-		//   }
-		// });
+		this.addCommand({
+			id: 'open-sample-modal-simple',
+			name: 'Open sample modal (simple)',
+			callback: () => {
+				new SampleModal(this.app).open();
+			}
+		});
 
 		// // This adds an editor command that can perform some operation on the current editor instance
 		// this.addCommand({
@@ -117,16 +114,14 @@ class SampleModal extends Modal {
 
 	onOpen() {
 		const { contentEl } = this;
-		// contentEl.setText("Woah!");
-		contentEl.textContent = "Woah!";
+		contentEl.setText("Woah!");
 
 		this.setTitle("Sample Modal");
 	}
 
 	onClose() {
 		const { contentEl } = this;
-		// contentEl.empty();
-		contentEl.innerHTML = "";
+		contentEl.empty();
 	}
 }
 
@@ -141,8 +136,7 @@ class SampleSettingTab extends PluginSettingTab {
 	display(): void {
 		const { containerEl } = this;
 
-		// containerEl.empty();
-		containerEl.innerHTML = "";
+		containerEl.empty();
 
 		new Setting(containerEl)
 			.setName('Setting #1')
